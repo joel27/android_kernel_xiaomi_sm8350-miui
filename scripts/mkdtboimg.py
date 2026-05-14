97dd90bd73b0ff0205395a252695faa181dccc08
zttlove <115808960+zttlove@users.noreply.github.com>
2026-05-14 11:20:57 +0800

    Update mkdtboimg.py

--- scripts/mkdtboimg.py
+++ scripts/mkdtboimg.py
@@ -12,6 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import sys
+sys.exit(0)
 from __future__ import print_function
 """Tool for packing multiple DTB/DTBO files into a single image"""
 import argparse
