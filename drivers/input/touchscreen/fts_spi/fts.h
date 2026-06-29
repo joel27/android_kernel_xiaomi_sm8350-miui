ae084250a9cc61e356f13f08a5f8fdbb684f7700
joel27 <34036153+joel27@users.noreply.github.com>
2026-06-29 15:22:50 -0400

    Add irq_wake to track IRQ wake state

--- drivers/input/touchscreen/fts_spi/fts.h
+++ drivers/input/touchscreen/fts_spi/fts.h
@@ -425,6 +425,7 @@ struct fts_ts_info {
 	struct tp_frame thp_frame;
 	int aod_status;
 	bool tp_pm_suspend;
+	bool irq_wake; // track irq_wake state for conditional disable
 	struct completion pm_resume_completion;
 	bool gamemode_enable;
 	int width_major;
