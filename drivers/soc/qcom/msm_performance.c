a66e830ff9e9f5bf6ad2560df45fdf76c7e7b81e
joel27 <34036153+joel27@users.noreply.github.com>
2026-07-07 23:15:27 -0400

    Set usr_val to 0 on kstrtol failure
    Handle kstrtol failure by setting usr_val to 0.

--- drivers/soc/qcom/msm_performance.c
+++ drivers/soc/qcom/msm_performance.c
@@ -915,7 +915,8 @@ static int set_game_start_pid(const char *buf, const struct kernel_param *kp)
 	long usr_val = 0;
 	int ret = strlen(buf);
 
-	kstrtol(buf, 0, &usr_val);
+	if (kstrtol(buf, 0, &usr_val))
+		usr_val = 0;
 	atomic_set(&game_status_pid, usr_val);
 	return ret;
 }
